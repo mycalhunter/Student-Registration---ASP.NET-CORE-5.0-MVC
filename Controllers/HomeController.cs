5c88b877e6c036c77e9bc2d2bc48c315c1a7b938
Hunter Miller <mycalhunter@gmail.com>
2021-10-22 22:17:06 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() // Home
+        {
+            return View();
+        }
+
+        public IActionResult Privacy() // Privacy
+        {
+            return View();
+        }
+
+        public IActionResult SignUp() // Registration
+        {
+            return View();
+        }
+
+        // POST - When registration form is submitted from /SignUp
+        [HttpPost]
+        public IActionResult SignUp(string Student)
+        {
+            if (ModelState.IsValid) // If form data is valid and authentic
+            {
+                return RedirectToAction("Index"); // redirect to home
+            } else
+            {
+                return RedirectToAction("SignUp"); // redirect to registration page
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StudentRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Models
+{
+    public class StudentRegistration
+    {
+        [Display(Name = "Student Name")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "Major")]
+        public string Major { get; set; }
+
+        [Display(Name = "Age")]
+        [Range(0, 99, ErrorMessage = "Please enter a valid Age")]
+        public int StudentAge { get; set; }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress", ErrorMessage = "The email address does not match.")]
+        public string ConfirmEmail { get; set; }
+    }
+}
